468209c3e447e6c1a54b9a3701190d1c6f45a51d
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-11-11 19:35:39 +0000

    Basic Deleting of a node with 1 child

--- AVL_BinaryTree.cpp
+++ AVL_BinaryTree.cpp
@@ -36,15 +36,50 @@ BinaryTree* insertNode(BinaryTree* node, int value){
 
 //Deleting a value from the node - means we need to search for that value and then..
 BinaryTree* deleteNode(BinaryTree* node, int value){
+    
+    //Note, should be reversed as needs to check if there are children from most to least children.
 
+    //case 1, no children - just remove;
+    if(node->left and node->right == NULL){
+        node->value = NULL;
+    }
+    else if(node->right != NULL){  //has child to the right
+        node->value = node->right->value;
+        node->right = NULL;
+    }
 
 
     return node;
 }
 
+//Taken from CUMOODLE - START
+void postorder(BinaryTree* tree) {
+	if (tree->left != NULL)
+		postorder(tree->left);
+	if (tree->right != NULL)
+		postorder(tree->right);
+	cout << tree->value << std::endl;
+
+}
+
+void in_order(BinaryTree* tree) {
+	if (tree->left != NULL)
+		in_order(tree->left);
+	cout << tree->value << std::endl;
+	if (tree->right != NULL)
+		in_order(tree->right);
+}
+//END
+
 int main(){
 
-
-
-    return 0;
+    BinaryTree* t = insertNode(0, 6);
+	insertNode(t, 10);
+	insertNode(t, 5);
+	insertNode(t, 2);
+	insertNode(t, 3);
+	insertNode(t, 4);
+	insertNode(t, 11);
+	in_order(t);
+	return 0;
 }
